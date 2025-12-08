435062351b24d071d1759ace4eb0a13d67e8e19b
Jeremy <jeremy.wengler@live.be>
2025-12-08 11:29:11 +0100

    1er config de base

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,14 @@
-import './bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
+ * This file will be included onto the page via the encore_entry_script_tags() Twig function.
  */
+
+// Import Bootstrap
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Import your custom CSS
 import './styles/app.css';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+console.log('This log comes from assets/app.js - welcome to Webpack Encore! 🎉');
