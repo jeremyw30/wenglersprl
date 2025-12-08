f90162d644d5692911a2811b0816b136f25a9ae6
Jeremy <jeremy.wengler@live.be>
2025-12-08 12:22:14 +0100

    configurate boostrapp

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,8 @@
 /*
  * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the encore_entry_script_tags() Twig function.
  */
-
-// Import Bootstrap
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Import your custom CSS
 import './styles/app.css';
 
-console.log('This log comes from assets/app.js - welcome to Webpack Encore! 🎉');
+console.log('WENGLER SPRL - Site loaded successfully');
